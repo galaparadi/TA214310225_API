b1c086d81f7f039f8c7808738a365a50dd8cea88
galaparadi <gala_koyin@yahoo.co.id>
2021-07-05 07:49:51 +0700

    ubah router path live chat

--- models/chat-model.js
+++ models/chat-model.js
@@ -2,14 +2,27 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const chatSchema = new Schema({
+    partisant: [String],
     workspace: String,
-    message: String,
+    chat: {
+        message: String,
+        sender: String,
+    },
     statusRead: {type: Boolean, default: false},
-    sender: String,
-    receiver: String,
     date: {type: Date, default: new Date()},
 });
 
+chatSchema.index({partisant: 1, workspace: 1});
+
+// chatSchema.set('autoIndex',false);
+
 const Chat = mongoose.model('chat', chatSchema);
+
+Chat.on('index', err => {
+    if(err){
+        console.log("error creating index for Chat model")
+        console.log(err)
+    }
+})
 
 module.exports = Chat;
