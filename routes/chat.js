3b651f4827997c2d8e8918478b7e5fc6ff8c8ee4
galaparadi <gala_koyin@yahoo.co.id>
2021-07-05 07:52:57 +0700

    ubah router controller chat

--- routes/chat.js
+++ routes/chat.js
@@ -2,16 +2,31 @@ const express = require('express');
 const router = express.Router();
 const Chat = require('../models/chat-model');
 
-router.post('/', (req, res) => {
-    Chat.create(req.body,(err,data) =>{
-        res.send(data);
-    })
+router.get('/', (req, res) => {
+    res.sendStatus(403);
+})
+
+router.post('/', (req, res, next) => {
+    req.body.partisant = req.body.partisant.sort();
+    Chat.create(req.body, (err, data) => {
+        if (err) next(err);
+        res.send({ data, status: 1 });
+    });
 });
 
-router.get('/', (req, res) => {
-    Chat.find({sender: 'dudung'}).sort({date: 'desc'}).exec().then(data => {
-        res.send(data);
-    })   
+router.get('/:username', (req, res) => {
+    let queryObject = {
+        partisant: {
+            $all: [req.params.username, req.query.username]
+        },
+        workspace: req.query.workspace
+    }
+
+    Chat.find(queryObject).exec().then(data => {
+        res.send({status: 1, data});
+    }).catch(err => {
+        next(err);
+    });
 });
 
 module.exports = router;
