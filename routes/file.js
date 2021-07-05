80056e1347630652221350c4eec855840be26c5d
galaparadi <gala_koyin@yahoo.co.id>
2021-07-05 08:38:29 +0700

    upload download by google drive

--- routes/file.js
+++ routes/file.js
@@ -3,37 +3,83 @@ var Grid = require('gridfs-stream');
 var express = require('express');
 var multer = require('multer');
 var stream = require('stream');
+let { google } = require('googleapis');
 var keys = require('../config/keys');
 var router = express.Router();
 const GridFsStorage = require('multer-gridfs-storage');
 var Document = require('../models/document-model');
 var Workspace = require('../models/workspace-model');
+const { response } = require('../app');
 
 const storage = multer.memoryStorage();
-var upload = multer({storage: storage});
+var upload = multer({ storage: storage });
 
-router.get('/', (req, res) => {	
+router.get('/', (req, res) => {
 	res.sendStatus(403);
 });
 
+router.post('/gdrive/upload/:id', (req, res, next) => {
+	const oAuth2Client = new google.auth.OAuth2(
+		keys.google.clientId,
+		keys.google.clientSecret,
+		"http://localhost/u/auth/google/redirect"); //redirect uri
+
+	oAuth2Client.setCredentials({
+		refresh_token: req.body.refreshToken,
+		scope: "https://www.googleapis.com/auth/drive.file",
+	});
+
+	const drive = google.drive({ version: 'v3', auth: oAuth2Client });
+
+	res.send(req.body);
+});
+
+router.get('/gdrive/download/:id', (req, res, next) => {
+	const oAuth2Client = new google.auth.OAuth2(
+		keys.google.clientId,
+		keys.google.clientSecret,
+		"http://localhost/u/auth/google/redirect"); //redirect uri
+
+	oAuth2Client.setCredentials({
+		refresh_token: req.query.refreshToken,
+		scope: "https://www.googleapis.com/auth/drive.file",
+	});
+
+	const drive = google.drive({ version: 'v3', auth: oAuth2Client });
+	drive.files.export({
+		fileId: req.params.id,
+		mimeType: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+	}, {
+		responseType: 'stream'
+	},
+		(err, response) => {
+			if (err) {
+				next(err);
+				return
+			}
+			response.data.pipe(res);
+		}
+	)
+});
+
 // how to catch error when downloading
-router.get('/download/:name',(req,res,next) => {
+router.get('/download/:name', (req, res, next) => {
 	var bucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db);
 	// var downloadstream = bucket.openDownloadStreamByName(req.params.name);
 	var downloadstream = bucket.openDownloadStream(mongoose.Types.ObjectId(req.params.name));
-	downloadstream.on('error', function(error){
+	downloadstream.on('error', function (error) {
 		console.log("something happen on downoadstream");
 		next(error);
 	});
 
 	downloadstream.pipe(res)
-	.on('error', function(error) {
-		console.log("terjadi error");
-		next(err);
-	})
-	.on('finish', function() {
-		console.log('done!');
-	});
+		.on('error', function (error) {
+			console.log("terjadi error");
+			next(err);
+		})
+		.on('finish', function () {
+			console.log('done!');
+		});
 })
 
 //belum sempurna. akan dijadikan transaction
@@ -48,23 +94,24 @@ router.post('/upload', upload.single('document'), (req, res, next) => {
 		next(err);
 	})
 
-	bufferstream.pipe(bucket.openUploadStreamWithId(id,req.body['filename'],{metadata : "dudung"}))
-	.on('finish', () => {
-		let doc = new Document({fsId : id,name: req.body['filename'],author:req.user.id});
-		
-		Workspace.findOne({name:req.body.workspace})
-		.exec()
-		.then(function(wp){	
-			wp.addDocument(doc);
-			res.redirect("/" + req.body.workspace);
+	bufferstream.pipe(bucket.openUploadStreamWithId(id, req.body['filename'], { metadata: "dudung" }))
+		.on('finish', () => {
+			let doc = new Document({ fsId: id, name: req.body['filename'], author: req.user.id });
+
+			Workspace.findOne({ name: req.body.workspace })
+				.exec()
+				.then(function (wp) {
+					wp.addDocument(doc);
+					res.redirect("/" + req.body.workspace);
+				})
+				.catch(function (err) {
+					next(err);
+				});
 		})
-		.catch(function(err) {
+		.on('error', (err) => {
+			console.log("terjadi error tak terduga");
 			next(err);
-		});
-	})
-	.on('error', () => {
-		console.log("terjadi error tak terduga");
-	})
+		})
 });
 
 module.exports = router;
