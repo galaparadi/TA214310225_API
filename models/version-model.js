258efd8d36b6e634bede83fb44630f22e4a15679
galaparadi <gala_koyin@yahoo.co.id>
2021-07-30 23:05:57 +0700

    tambah autoincrement dan index di Model

--- models/version-model.js
+++ models/version-model.js
@@ -3,11 +3,21 @@ const Schema = mongoose.Schema;
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
 const versionSchema = new Schema({
-	documentId : {type : Schema.Types.ObjectId},
-	version : Number,
-	status : String
+	documentId: ObjectId,
+	version: Number,
+	author: String,
 });
 
-const Version = mongoose.model('version',versionSchema);
+versionSchema.pre('save', async function (next) {
+	console.log('this version schema is saving...');
+	let { version: lastVersion } = await mongoose.model('version', this.schema).findOne({ documentId: this.documentId }).sort({ version: -1 }).exec();
+	this.version = lastVersion + 1
+	console.log(this);
+	next()
+})
+
+versionSchema.index({ documentId: 1, version: -1 });
+
+const Version = mongoose.model('version', versionSchema);
 
 module.exports = Version;
