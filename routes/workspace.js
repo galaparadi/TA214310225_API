3d4cf047e036d92b3b5166389e4a1bd3212eddfe
galaparadi <gala_koyin@yahoo.co.id>
2021-08-21 10:48:40 +0700

    code refactoring

--- routes/workspace.js
+++ routes/workspace.js
@@ -7,8 +7,10 @@ const upload = multer({ storage: storage });
 
 router.use((req, res, next) => {
     //TODO: bearer auth
+    //TODO: role check
     next();
 });
+
 router.get('/', (req, res) => { res.status(403).json({ message: "Forbidden" }) })
     .post('/', WorkspaceAPIController.addWorkspace)
     .get('/:name', WorkspaceAPIController.getWorkspace)
@@ -17,19 +19,25 @@ router.get('/', (req, res) => { res.status(403).json({ message: "Forbidden" }) }
     .get('/:name/feeds', WorkspaceAPIController.getFeeds)
     .get('/:name/users', WorkspaceAPIController.getUsers)
     .post('/:name/users', WorkspaceAPIController.addUser)
-    .get('/:name/documents', WorkspaceAPIController.getDocuments)
-    .post('/:name/documents', upload.single('file'), WorkspaceAPIController.addDocument)
-    .get('/:name/documents/:docid', WorkspaceAPIController.getDocument)
-    .get('/:name/documents/:docid/file', WorkspaceAPIController.getDocumentFile)
-    .get('/:name/documents/:docid/versions', WorkspaceAPIController.getDocumentVersions)
-    .get('/:name/documents/:docid/versions/:version', WorkspaceAPIController.getDocumentVersion)
-    .post('/:name/documents/:docid/versions', upload.single('file'), WorkspaceAPIController.addDocumentVersion)
-    .post('/:name/documents/:docid/versions/submit', upload.single('file'), WorkspaceAPIController.askAddDocumentVersion)
-    .post('/:name/documents/:docid/comments', WorkspaceAPIController.addComment)
-    .get('/:name/documents/:docid/comments', WorkspaceAPIController.getComments)
     .post('/:name/filetree', WorkspaceAPIController.addFiletree)
     .put('/:name/documents', WorkspaceAPIController.updateDocument)
     .put('/:name/filetree', WorkspaceAPIController.addFiletree)
     .delete('/:name/user', WorkspaceAPIController.deleteUser)
+
+//document manage endpoint
+router.get('/:name/documents', WorkspaceAPIController.getDocuments)
+    .post('/:name/documents', upload.single('file'), WorkspaceAPIController.addDocument)
+    .get('/:name/documents/:docid', WorkspaceAPIController.getDocument)
+    .get('/:name/documents/:docid/file', WorkspaceAPIController.getDocumentFile)
+    .get('/:name/documents/:docid/versions', WorkspaceAPIController.getDocumentVersions)
+    .get('/:name/documents/:docid/file/versions/:version', WorkspaceAPIController.getDocumentVersion)
+    .post('/:name/documents/:docid/versions', upload.single('file'), WorkspaceAPIController.addDocumentVersion)
+    .post('/:name/documents/:docid/versions/submit', upload.single('file'), WorkspaceAPIController.askAddDocumentVersion)
+    .post('/:name/documents/:docid/comments', WorkspaceAPIController.addComment)
+    .get('/:name/documents/:docid/comments', WorkspaceAPIController.getComments)
+
+router.get('/test/:id', (req, res, next) => {
+    res.send('woosh')
+})
 
 module.exports = router;
